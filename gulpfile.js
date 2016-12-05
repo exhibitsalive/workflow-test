50f08a8b87129adb813d7af51ad74ef107079eb0
Mark Fowle <mark@exhibitsalive.com>
2016-12-04 17:39:05 -0800

    Concatenated Javascript added

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require("gulp");
 var gutil = require("gulp-util");
 var coffee = require("gulp-coffee");
+var concat = require("gulp-concat");
 var coffeeSource = ["components/coffee/tagline.coffee"];
-
+var jsSource = [
+                "components/scripts/rclick.js",
+                "components/scripts/pixgrid.js",
+                "components/scripts/tagline.js",
+                "components/scripts/template.js"
+               ]
 gulp.task("coffee", function(){
     gulp.src(coffeeSource)
     .pipe(coffee({bare:true})
         .on("error", gutil.log))
     .pipe(gulp.dest("components/scripts"))
+});
+gulp.task("js", function(){
+    gulp.src(jsSource)
+    .pipe(concat("script.js"))
+    .pipe(gulp.dest("builds/development/js"))
+    
 });
