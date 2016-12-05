b47b609331ab766c0aeb1d2b709e8cabcd9644b6
Mark Fowle <mark@exhibitsalive.com>
2016-12-05 09:12:42 -0800

    Compass added

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,15 @@ var gutil = require("gulp-util");
 var coffee = require("gulp-coffee");
 var concat = require("gulp-concat");
 var browserify = require("gulp-browserify");
+var compass = require("gulp-compass");
 var coffeeSource = ["components/coffee/tagline.coffee"];
 var jsSource = [
     "components/scripts/rclick.js",
     "components/scripts/pixgrid.js",
     "components/scripts/tagline.js",
     "components/scripts/template.js"
-    ]
+    ];
+var sassSource = ["components/sass/style.scss"];
 gulp.task("coffee", function(){
     gulp.src(coffeeSource)
     .pipe(coffee({bare:true})
@@ -22,4 +24,14 @@ gulp.task("js", function(){
     .pipe(browserify())
     .pipe(gulp.dest("builds/development/js"))
     
+});
+gulp.task("compass", function(){
+    gulp.src(sassSource)
+    .pipe(compass({
+        sass:"components/sass",
+        images:"build/development/images",
+        style:"expanded"
+    }))
+    .on("error", gutil.log)
+    .pipe(gulp.dest("builds/development/css"))
 });
