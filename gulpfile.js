a2a2f1a84f814340e189896b443951dcbb3db2d0
Mark Fowle <mark@exhibitsalive.com>
2016-12-05 08:27:35 -0800

    Browserify Jquery and Mustache added

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var coffee = require("gulp-coffee");
 var concat = require("gulp-concat");
+var browserify = require("gulp-browserify");
 var coffeeSource = ["components/coffee/tagline.coffee"];
 var jsSource = [
-                "components/scripts/rclick.js",
-                "components/scripts/pixgrid.js",
-                "components/scripts/tagline.js",
-                "components/scripts/template.js"
-               ]
+    "components/scripts/rclick.js",
+    "components/scripts/pixgrid.js",
+    "components/scripts/tagline.js",
+    "components/scripts/template.js"
+    ]
 gulp.task("coffee", function(){
     gulp.src(coffeeSource)
     .pipe(coffee({bare:true})
@@ -18,6 +19,7 @@ gulp.task("coffee", function(){
 gulp.task("js", function(){
     gulp.src(jsSource)
     .pipe(concat("script.js"))
+    .pipe(browserify())
     .pipe(gulp.dest("builds/development/js"))
     
 });
